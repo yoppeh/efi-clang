43782caac1b73ad28b9a47e4194265e78a720179
Warren Mann <warrenm@gmail.com>
2018-05-05 14:56:58 -0500

    petoefi is not necessary

--- link.sh
+++ link.sh
@@ -1,5 +1,5 @@
 ar=("$@")
-lld-link -subsystem:console -nodefaultlib -dll -entry:efi_main ${ar[@]:1} -out:${ar[0]}.dll
+lld-link -subsystem:EFI_APPLICATION -nodefaultlib -dll -entry:efi_main ${ar[@]:1} -out:${ar[0]}.dll
 petoefi ${ar[0]}.dll ${ar[0]} app
 rm ${ar[0]}.lib
 rm ${ar[0]}.dll
